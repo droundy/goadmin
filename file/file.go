56b1a06c8a6f31399e55e0ac4a735eff4bf5e220
David Roundy <roundyd@physics.oregonstate.edu>
2010-08-28 12:33:12 -0400

    add ability to copy a directory recursively.
    Tested-on: collins (Linux 2.6.32-5-amd64)

--- file/file.go
+++ file/file.go
@@ -3,9 +3,9 @@ package file
 import (
 	"os"
 	"io"
+	"path"
 	"io/ioutil"
 	"fmt"
-	"syscall"
 	"strconv"
 	"github.com/droundy/goadmin/ago"
 	"github.com/droundy/goadmin/gomakefile"
@@ -41,29 +41,175 @@ type File struct {
 	Contents string
 }
 
-func (f File) Set(which Field) (needswriting bool, e os.Error) {
-	stat, e := os.Lstat(f.Name)
-	if e == nil {
-		switch stat.Mode & syscall.S_IFMT {
-		case syscall.S_IFLNK:
-			e = os.Remove(f.Name)
-			if e != nil { return false, e }
-		case syscall.S_IFREG:
-			// Just an ordinary file.
-			if Perms & which != 0 && stat.Permission() != f.Perms {
-				e = os.Chmod(f.Name, f.Perms)
-				if e != nil { return false, e }
-			} else {
-				f.Perms = stat.Permission()
-			}
-			// Set the ownership if needed...
-			if Gid & which == 0 { f.Gid = stat.Gid }
-			if Uid & which == 0 { f.Uid = stat.Uid }
-			e = os.Lchown(f.Name, f.Uid, f.Gid)
-			if e != nil { return false, e }
-		case syscall.S_IFDIR:
-			return false, os.NewError(f.Name+" is a directory, not a file!")
+type Directory struct {
+	StatData
+	Contents map[string]StatEntry
+}
+
+type StatEntry interface {
+	Set(Field) (needswriting bool, e os.Error)
+	GoSet(Field)
+	Move(from, to string)
+}
+
+func (f *StatData) Move(from, to string) {
+	from = path.Clean(from)+"/"
+	to = path.Clean(to)
+	if f.Name == "" {
+		// This is silly!
+		return
+	}
+	if from == "./" {
+		if f.Name[0] != '/' {
+			f.Name = path.Join(to, f.Name)
 		}
+	} else if len(f.Name) > len(from) && f.Name[0:len(from)] == from {
+		f.Name = path.Join(to, f.Name[len(from):])
+	}
+}
+
+func (f *Directory) Move(from, to string) {
+	f.StatData.Move(from, to)
+	for x := range f.Contents {
+		f.Contents[x].Move(from, to)
+	}
+}
+
+// *StatData Set modifies the StatData to reflect that actual stats of
+// the file, when it isn't instructed to set those values.  Thus it is
+// an input/output method that can be used to ensure that all the
+// stats of the *StatData are correct, based on the current stats of
+// the file or directory.
+
+func (f *StatData) Set(which Field) (needswriting bool, e os.Error) {
+	stat, e := Stat(f.Name)
+	if e != nil {
+		_, ispatherror := e.(*os.PathError) // *os.PathError means file doesn't exist (which is okay)
+		if e != nil && !ispatherror { return false, e }
+		return true, nil
+	}
+	if stat.Mode != f.Mode {
+		return true, os.Remove(f.Name)
+	}
+	if Perms & which != 0 && stat.Perms != f.Perms {
+		e = os.Chmod(f.Name, f.Perms)
+		if e != nil { return false, e }
+	} else {
+		f.Perms = stat.Perms
+	}
+	// Set the ownership if needed...
+	if Gid & which == 0 { f.Gid = stat.Gid }
+	if Uid & which == 0 { f.Uid = stat.Uid }
+	if Uid & which != 0 || Gid & which != 0 {
+		e = os.Lchown(f.Name, f.Uid, f.Gid)
+		if e != nil { return false, e }
+	}
+	return false, nil
+}
+
+func (ent *StatData) Read() (f StatEntry, e os.Error) {
+	switch ent.Mode {
+	case IsFile:
+		ff, e := ent.readFile()
+		if e != nil {
+			return nil, e
+		} else {
+			return &ff, e
+		}
+	case IsDirectory:
+		ff, e := ent.readDirectory()
+		if e != nil {
+			return nil, e
+		} else {
+			return &ff, e
+		}
+	default:
+		return nil, os.NewError("I only know how to read files and directories so far.")
+	}
+	return
+}
+
+func (st *StatData) readFile() (f File, e os.Error) {
+	f.StatData = *st
+	if f.Mode != IsFile {
+		return f, os.NewError("Not a regular file:  "+f.Name)
+	}
+	x, e := ioutil.ReadFile(f.Name)
+	f.Contents = string(x)
+	return
+}
+
+func (st *StatData) readDirectory() (f Directory, e os.Error) {
+	f.StatData = *st
+	if f.Mode != IsDirectory {
+		return f, os.NewError("Not a directory:  "+f.Name)
+	}
+	x, e := os.Open(f.Name, os.O_RDONLY, f.Perms)
+	if e != nil { return }
+	fs, e := x.Readdirnames(-1)
+	f.Contents = make(map[string]StatEntry)
+	for _, fname := range fs {
+		if fname != "." && fname != ".." {
+			ent, e := Stat(path.Join(f.Name, fname))
+			if e != nil { return f, e }
+			xxx, _ := ent.Read()
+			f.Contents[fname] = xxx
+		}
+	}
+	return
+}
+
+func (f *Directory) Set(which Field) (needswriting bool, e os.Error) {
+	needswriting, e = f.StatData.Set(which)
+	if e != nil { return }
+	if needswriting {
+		// This means it doesn't currently exist...
+		e = os.Mkdir(f.Name, f.Perms)
+		if e != nil { return }
+		// Set owner...
+		e = os.Lchown(f.Name, f.Uid, f.Gid)
+		if e != nil { return false, e }
+		// Chmod it in any case, because umask may have modified the
+		// desired permissions.
+		e = os.Chmod(f.Name, f.Perms)
+		if e != nil { return false, e }
+	}
+	if Contents & which != 0 {
+		for _, c := range f.Contents {
+			// Set all the contents as well!
+			c.Set(which)
+		}
+	}
+	return
+}
+
+func (f *Directory) GoSet(which Field) {
+	if which == 0 { return } // Nothing to set!
+	ago.Import("github.com/droundy/goadmin/file")
+	ago.Declare("var changed_files = make(map[string]bool)")
+	ago.Declare("var file_changed = false")
+	ago.Declare("var this_file_changed = false")
+	ago.Declare("var myd file.Directory")
+	code := "myd = file.Directory{file.StatData{"+strconv.Quote(f.Name)+","
+	code += fmt.Sprint(f.Uid, ",", f.Gid, ", 0", strconv.Itob(int(f.Perms), 8), ", file.IsFile},")
+	code += `map[string]file.StatEntry{}}`
+	code += fmt.Sprint("\n\tthis_file_changed, e = myd.Set(", which, ")")
+	code += fmt.Sprint("\n\tif e != nil { return }")
+	code += fmt.Sprint("\n\tif this_file_changed { changed_files[`",f.Name,"`] = true }")
+	code += fmt.Sprint("\n\tfile_changed = file_changed || this_file_changed")
+	ago.Code(code)
+	if Contents & which != 0 {
+		for _,x := range f.Contents {
+			x.GoSet(which)
+		}
+	}
+}
+
+func (f *File) Set(which Field) (needswriting bool, e os.Error) {
+	needswriting, e = f.StatData.Set(which)
+	if e != nil { return }
+	{
+		// Let's clear out any existing ".new" file
 		e = os.Remove(f.Name+".new")
 		_, ispatherror := e.(*os.PathError) // *os.PathError means .new file doesn't exist (which is okay)
 		if e != nil && !ispatherror { return false, e }
@@ -111,32 +257,28 @@ func Stat(n string) (f StatData, e os.Error) {
 	return
 }
 
-func Read(n string) (f File, e os.Error) {
-	f.StatData, e = Stat(n)
+func Read(n string) (f StatEntry, e os.Error) {
+	st, e := Stat(n)
 	if e != nil { return }
-	if f.Mode != IsFile {
-		return f, os.NewError("Not a regular file:  "+n)
-	}
-	x, e := ioutil.ReadFile(f.Name)
-	f.Contents = string(x)
-	return
+	return st.Read()
 }
 
-func (f File) GoSet(which Field) {
+func (f *File) GoSet(which Field) {
 	if which == 0 { return } // Nothing to set!
 	ago.Import("github.com/droundy/goadmin/file")
 	ago.Declare("var changed_files = make(map[string]bool)")
 	ago.Declare("var file_changed = false")
 	ago.Declare("var this_file_changed = false")
-	code := "this_file_changed, e = file.File{file.StatData{"+strconv.Quote(f.Name)+","
-	code += fmt.Sprint(f.Uid, ",", f.Gid, ",", strconv.Itob(int(f.Perms), 8), ", file.IsFile},")
+	ago.Declare("var myf file.File")
+	code := "myf = file.File{file.StatData{"+strconv.Quote(f.Name)+","
+	code += fmt.Sprint(f.Uid, ",", f.Gid, ", 0", strconv.Itob(int(f.Perms), 8), ", file.IsFile},")
 	if Contents & which != 0 {
 		code += strconv.Quote(f.Contents) + "}"
 	} else {
 		code += `""}`
 	}
-	code += fmt.Sprint(".Set(", which, ")")
-	code += fmt.Sprint("\n\tif e != nil { return }")
+	code += fmt.Sprint("\n\tthis_file_changed, e = myf.Set(", which, ")")
+	code += fmt.Sprint("\n\tif e != nil { fmt.Println(`Trouble setting",f.Name,":`,e); return }")
 	code += fmt.Sprint("\n\tif this_file_changed { changed_files[`",f.Name,"`] = true }")
 	code += fmt.Sprint("\n\tfile_changed = file_changed || this_file_changed")
 	ago.Code(code)
--- test/asroot/file.go.go
+++ test/asroot/file.go.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/droundy/goadmin/file"
 	"github.com/droundy/goadmin/ago"
 )
@@ -11,7 +12,27 @@ func main() {
 	// Want all machines to have the same /etc/motd
 	motd,_ := file.Read("/etc/motd")
 	motd.GoSet(file.All)
-	file.File{file.StatData{"/tmp/test-from-asroot", 0, 0, 0666, file.IsFile}, "Some demo contents\n"}.GoSet(file.All)
+	tmp := file.File{file.StatData{"/tmp/test-from-asroot", 0, 0, 0666, file.IsFile}, "Some demo contents\n"}
+	tmp.GoSet(file.All)
+
+	demofiles,e := file.Read("demo-files")
+	if e == nil {
+		fmt.Println("I am going output demo-files");
+		demofiles.Move(".", "/tmp")
+		demofiles.GoSet(file.All)
+	} else {
+		fmt.Println("Error reading demo-files: ", e)
+	}
+
+	subdir,e := file.Read("demo-files/subdir")
+	if e == nil {
+		fmt.Println("I am going output demo-files/subdir");
+		subdir.Move("demo-files", "/tmp")
+		subdir.GoSet(file.All)
+	} else {
+		fmt.Println("Error reading subdir: ", e)
+	}
+
 	ago.Code(`if file_changed { fmt.Println("I changed a file!") }`)
 	ago.Print("files")
 }
